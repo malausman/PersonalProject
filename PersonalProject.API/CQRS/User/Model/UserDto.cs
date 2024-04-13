68819431f5b30d23e8b2db52c6f10ed596739e59
HP <HP@DESKTOP-KBRJR8U>
2024-04-13 21:37:30 +0500

    next is jwt

--- PersonalProject.API/CQRS/User/Model/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿
-    public class RoleDto
-    {
-        public string user_id { get; set; }
-        public bool is_active { get; set; }
-        public DateTime  created_date { get; set; }
-        
-    }
-
